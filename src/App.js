448ba9d88c37bb5f8341c03f038ec7bfe2a9117f
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-05-10 23:53:47 +0530

    call welcome object in h1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import * as React from "react";
-function App() {
-  const title = "React";
 
+const welcome = {
+  greeting: "Hey",
+  title: "React",
+};
+
+function App() {
   return (
     <div>
-      <h1>Hello {title}</h1>
+      <h1>
+        {welcome.greeting} {welcome.title}
+      </h1>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" />
     </div>
   );
 }
