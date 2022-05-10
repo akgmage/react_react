dec7a418a9e5146d24407a3495abf96b8dd2ccc2
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-05-10 23:47:48 +0530

    Add title

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import React from "react";
+import * as React from "react";
 function App() {
+  const title = "React";
+
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1>Hello {title}</h1>
     </div>
   );
 }
