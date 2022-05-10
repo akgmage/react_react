b9ad0a8303b513f25331eeece11d7070eadb7095
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-05-10 23:55:35 +0530

    Replace object with function and call function in h1

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import * as React from "react";
 
-const welcome = {
-  greeting: "Hey",
-  title: "React",
-};
+function getTitle(title) {
+  return title;
+}
 
 function App() {
   return (
     <div>
-      <h1>
-        {welcome.greeting} {welcome.title}
-      </h1>
+      <h1>Hello {getTitle("React")}</h1>
       <label htmlFor="search">Search: </label>
       <input id="search" type="text" />
     </div>
