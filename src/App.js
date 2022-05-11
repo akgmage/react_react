b25f744b0af1b40b4ba65e3817edb9c79b9ff9b1
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-05-11 23:16:58 +0530

    Display more of item properties

--- src/App.js
+++ src/App.js
@@ -28,7 +28,16 @@ function App() {
 
       <hr />
       {list.map(function (item) {
-        return <li key={item.objectID}>{item.title}</li>;
+        return (
+          <li key={item.objectID}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span> {item.author} </span>
+            <span> {item.num_comments} </span>
+            <span> {item.points} </span>
+          </li>
+        );
       })}
     </div>
   );
