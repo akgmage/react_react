c6bf0b55280925db80ef4b764eed159c0b0e1eb6
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-05-11 23:01:24 +0530

    Create list of objects

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import * as React from "react";
 
-function getTitle(title) {
-  return title;
-}
+const list = [
+  {
+    title: "React",
+    url: "https://reactjs.org/",
+    author: "Jordan Walke",
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: "Redux",
+    url: "https://reduxjs.org/",
+    author: "Dan Abramov, Andrew Clark",
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
 
 function App() {
   return (
     <div>
-      <h1>Hello {getTitle("React")}</h1>
+      <h1>Hello</h1>
       <label htmlFor="search">Search: </label>
       <input id="search" type="text" />
     </div>
