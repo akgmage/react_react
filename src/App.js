75fde081f3176db5badddf3d5d81f31395a5c9ea
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-05-11 23:26:41 +0530

    Extract Search component

--- src/App.js
+++ src/App.js
@@ -39,14 +39,23 @@ function App() {
   return (
     <div>
       <h1>My Hacker Stories</h1>
-      <label htmlFor="search">Search: </label>
-      <input id="search" type="text" />
+
+      <Search />
 
       <hr />
 
       <List />
     </div>
   );
+
+  function Search() {
+    return (
+      <div>
+        <label htmlFor="search">Search: </label>
+        <input id="search" type="text" />
+      </div>
+    );
+  }
 
   function List() {
     return (
