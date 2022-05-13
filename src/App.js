aad2c767d9077811fa6a6610526f1a50e06bbc13
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-05-13 23:39:45 +0530

    Extract Item Component and pass each item along to the List component

--- src/App.js
+++ src/App.js
@@ -63,19 +63,21 @@ const Search = () => {
 
 const List = (props) => (
   <ul>
-    {props.list.map((item) => {
-      return (
-        <li key={item.objectID}>
-          <span>
-            <a href={item.url}>{item.title}</a>
-          </span>
-          <span> {item.author} </span>
-          <span> {item.num_comments} </span>
-          <span> {item.points} </span>
-        </li>
-      );
-    })}
+    {props.list.map((item) => (
+      <Item key={item.objectID} item={item} />
+    ))}
   </ul>
+);
+
+const Item = (props) => (
+  <li>
+    <span>
+      <a href={props.item.url}>{props.item.title}</a>
+    </span>
+    <span> {props.item.author} </span>
+    <span> {props.item.num_comments} </span>
+    <span> {props.item.points} </span>
+  </li>
 );
 
 export default App;
